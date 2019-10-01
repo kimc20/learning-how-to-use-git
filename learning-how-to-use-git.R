717b2e6389cb823650d3586c3f8bf824275ddceb
Christian Kim <kimc20@mail.wlu.edu>
2019-10-01 12:42:04 -0400

    Hi

---
+++ learning-how-to-use-git.R
@@ -0,0 +1,6 @@
+# Commit in R Studio will only commit changes to files that are checked under the Git tab
+
+# WRITE COMMIT MESSAGES EVERYTIME YOU COMMIT!
+
+# Let's make a plot
+plot (c(1,2,3,4), c(5,6,7,8))
